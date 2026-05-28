d1868ae15d2fa7a2753b5ae0d928940edaa15e84
murali dangeti <muralidangeti2@gmail.com>
2026-05-28 12:54:09 +0530

    updation

--- testscenario1.spec.js
+++ testscenario1.spec.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 const { TIMEOUT } = require('dns');
 
-let a = "sidhu";
+let a = sidhu;
 //Setup
 const BASE_URL = "https://eventhub.rahulshettyacademy.com";
 const user_email = "sidhu@gmail.com";
